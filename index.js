684db787bb7c1a3e13253a7eb1a4977919bafb6f
federicoherrera <fede.juan.herrera@gmail.com>
2024-01-07 17:28:14 -0300

    ..

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import express from 'express'
+import pg from 'pg'
+import { config } from 'dotenv'
+
+config()
 
 const app = express()
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+})
 
 app.get('/', (req, res) => {
     res.json('Hola la concha de tu re putisima madre hijo de re mil re putras')
+})
+
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
 })
 
 app.listen(3000)
